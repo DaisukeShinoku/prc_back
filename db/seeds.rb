4e28c4f0893ad2dda3cab578683fa66330b1c0d6
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-08-14 10:03:20 +0900

    [Add]required_exp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+common_table_name = %w(required_exp)
+common_table_name.each do |table_name|
+  path = Rails.root.join('db', 'seeds', "#{table_name}.rb")
+  if File.exist?(path)
+    puts "Creating #{table_name}....."
+    require(path)
+  end
+end
