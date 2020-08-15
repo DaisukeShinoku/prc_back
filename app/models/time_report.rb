ee7422c678f526d1c5a4ff8223e469b692471ae1
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-08-15 11:37:01 +0900

    [Add]Tag Recorder

--- app/models/time_report.rb
+++ app/models/time_report.rb
@@ -6,8 +6,15 @@ class TimeReport < ApplicationRecord
 
   scope :join_exp, -> { joins(:experience_record)
     .select('time_reports.*, experience_records.*') }
+  scope :join_tags, -> { left_joins(:tags)
+    .group('time_reports.id, experience_records.id')
+    .select('time_reports.*, experience_records.*, ARRAY_AGG(tags.name) AS tags') }
   scope :newest, -> { order(created_at: :desc) }
 
   validates :study_time, presence: true
   validates :memo, length: { maximum: 280 }
+
+  def links
+    time_report_tag_links
+  end
 end
